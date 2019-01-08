adb6b47be1ae07b2814fd37171667991e118e1c5
Sigrid Benezra <sdbenezra@gmail.com>
2019-01-08 00:57:03 -0800

    Add cors-headers app to eliminate CORS problem

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'core',
     'user',
     'work',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,5 @@ MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
+
+CORS_ORIGIN_ALLOW_ALL = True
