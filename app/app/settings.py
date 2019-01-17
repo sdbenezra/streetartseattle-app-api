5e841e101f0adcff04d54bba735d9cb303c77328
Sigrid Benezra <sdbenezra@gmail.com>
2019-01-17 15:44:42 -0800

    Changed image upload to AWS S3 Bucket

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'user',
     'work',
     'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -127,11 +128,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+USE_S3 = os.environ.get('USE_S3') == 'TRUE'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/vol/web/static/'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = '/vol/web/media'
+
+STATICFILES_DIRS = ['/vol/web/static/']
+
 
 AUTH_USER_MODEL = 'core.User'
 
